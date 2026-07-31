493053271f4cbe2c1f7b9e8330b7c669b0907b74
Dai <2361776323@qq.com>
2026-07-31 23:20:44 +0800

    Sync Clean_Car_Tracking project

--- Debug/src/pid.d
+++
@@ -1,4 +0,0 @@
-# FIXED
-
-src/pid.o: ../src/pid.c C:/Users/l/workspace_ccstheia/ball\ PID/inc/pid.h
-C:/Users/l/workspace_ccstheia/ball\ PID/inc/pid.h:
--- Debug/src/safty.d
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-src/safty.o: ../src/safty.c \
- C:/Users/l/workspace_ccstheia/ball\ PID/inc/safty.h \
- C:/Users/l/workspace_ccstheia/ball\ PID/inc/app_config.h \
- C:/Users/l/workspace_ccstheia/ball\ PID/inc/vision.h
-C:/Users/l/workspace_ccstheia/ball\ PID/inc/safty.h:
-C:/Users/l/workspace_ccstheia/ball\ PID/inc/app_config.h:
-C:/Users/l/workspace_ccstheia/ball\ PID/inc/vision.h:
